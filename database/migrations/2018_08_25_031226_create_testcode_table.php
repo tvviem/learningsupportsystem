1ca29f874daa4301af074bece967771215500b02
viem trieu <trieuvinhviem@gmail.com>
2018-09-08 09:47:40 +0700

    Migrate perfect Database with RBAC design

--- database/migrations/2018_08_25_031226_create_testcode_table.php
+++ database/migrations/2018_08_25_031226_create_testcode_table.php
@@ -18,13 +18,13 @@ class CreateTestcodeTable extends Migration
         Schema::create('testcode', function (Blueprint $table) {
             $table->increments('id');
             $table->char('code', 16)->unique(); // Ma bo de
-            $table->tinyInteger('num_of_question', 2);
-            $table->varchar('encode_list_id', 100); // Ma hoa cac question_id trong bo de
+            $table->tinyInteger('num_of_question');
+            $table->string('encode_list_id', 100); // Ma hoa cac question_id trong bo de
             // tạo ngẫu nhiên như salt (code_rand) và kết hợp với mật mã
             // của giảng viên cung cấp khi tạo đề, Dùng để giải mã list_question_id
             $table->binary('salt', 32); // RandomSalt + user' key -> Encrypt with AES-256-CBC
             // want join to test; DESCRYPT
-            $table->tinyInteger('duration', 2); // Thoi gian lam bai cua bo de
+            $table->tinyInteger('duration'); // Thoi gian lam bai cua bo de
             
             $table->integer('user_id')->unsigned(); // Giang vien ra bo de kiem tra
             $table->foreign('user_id')->references('id')->on('users'); 
