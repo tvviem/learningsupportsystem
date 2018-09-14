e8108a2fb4a5362d4babf1222908d172f999870c
viem trieu <trieuvinhviem@gmail.com>
2018-09-14 09:54:55 +0700

    Basic add template adminLTE 3 alpha, add lib js, css and laravel mix

---
+++ resources/lang/vi/passwords.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'password' => 'Mật khẩu phải có ít nhất 6 ký tự và phải trùng khớp.',
+    'reset' => 'Mật khẩu đã được cập nhật.',
+    'sent' => 'Hệ thống đã gửi thư xác thực thay đổi mật khẩu đến hộp thư của bạn.',
+    'token' => 'Mã cho phép cập nhật mật khẩu không hợp lệ.',
+    'user' => "Hệ thống không tìm thấy định danh này.",
+];
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,6 @@
     return view('welcome');
 }); */
 Route::resource('/', 'HomeController');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
