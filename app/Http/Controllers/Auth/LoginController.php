73d45f1adf4cd704074b1d533f3cef56874f6f4d
viem trieu <trieuvinhviem@gmail.com>
2018-10-07 15:09:16 +0700

    Allow login with username or email

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -41,9 +41,12 @@ class LoginController extends Controller
 
     // Override login from AuthenticatesUsers
     public function login(Request $request) {
-        if(auth()->attempt(['email'=>$request->email,'password'=>$request->password])){
-            // dd(auth()->user()->active==1);
+        /* $identity  = request()->get('identity');
+        $fieldName = filter_var($identity, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+        $request->merge([$fieldName => $identity]); */
 
+        if(auth()->attempt($request->only($this->username(), 'password'))){
+            // dd(auth()->user()->active==1);
             if(auth()->user()->active==0){
                 Auth::logout();
                 return back()->with('warning', 'Your account has not yet been activated. Please check Your email');
@@ -52,5 +55,49 @@ class LoginController extends Controller
         } else {
             return back()->withInput()->with('error', 'Maybe Account informations are incorrect.');
         }
+    }
+
+    /**
+     * Check either username or email.
+     * @return string
+     */
+    public function username()
+    {
+        $identity  = request()->get('identity');
+        $fieldName = filter_var($identity, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+        request()->merge([$fieldName => $identity]);
+        return $fieldName;
+    }
+    /**
+     * Validate the user login.
+     * @param Request $request
+     */
+    protected function validateLogin(Request $request)
+    {
+        $this->validate(
+            $request,
+            [
+                'identity' => 'required|string',
+                'password' => 'required|string',
+            ],
+            [
+                'identity.required' => 'Username or email is required',
+                'password.required' => 'Password is required',
+            ]
+        );
+    }
+    
+    /**
+     * @param Request $request
+     * @throws ValidationException
+     */
+    protected function sendFailedLoginResponse(Request $request)
+    {
+        $request->session()->flash('login_error', trans('auth.failed'));
+        throw ValidationException::withMessages(
+            [
+                'error' => [trans('auth.failed')],
+            ]
+        );
     }
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,26 +11,31 @@
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
                         @csrf
+                        @if(session()->has('login_error'))
+                            <div class="alert alert-warning">
+                                {{ session()->get('login_error') }}
+                            </div>
+                        @endif
 
-                        <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                        <div class="form-group row {{ $errors->has('identity') ? ' has-error' : '' }}">
+                            <label for="identity" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address or Username') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+                                <input id="identity" type="identity" class="form-control" name="identity" value="{{ old('identity') }}" required autofocus>
 
-                                @if ($errors->has('email'))
+                                @if ($errors->has('identity'))
                                     <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
+                                        <strong>{{ $errors->first('identity') }}</strong>
                                     </span>
                                 @endif
                             </div>
                         </div>
 
-                        <div class="form-group row">
+                        <div class="form-group row {{ $errors->has('password') ? ' has-error' : '' }}">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                                <input id="password" type="password" class="form-control" name="password" required>
 
                                 @if ($errors->has('password'))
                                     <span class="invalid-feedback" role="alert">
